4cec6585f1d1b7e6cddac81e5ea2fdc6d4778c07
1504Rohit <rohitgiri945@gmail.com>
2024-11-22 18:43:59 +0530

    kfrf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "337719603"
+    serviceWorkerVersion: "2355793982"
   }
 });
