77a56a8290427abb012cb9edec655728149e9ba6
1504Rohit <rohitgiri945@gmail.com>
2024-11-25 09:06:03 +0530

    nnfrf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1524891237"
+    serviceWorkerVersion: "3415163716"
   }
 });
